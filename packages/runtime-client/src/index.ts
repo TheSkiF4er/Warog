349479035bebacc0070661ed43d40d5886736617
TheSkiF4er <cajeerskif4er@gmail.com>
2025-12-11 10:15:28 +0300

    Add files via upload

---
+++ packages/runtime-client/src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Точка входа клиентского рантайма Warog.
+ * Переэкспорт ключевых адаптеров.
+ */
+
+export * from './adapters/dom-adapter';
+export * from './adapters/fetch-adapter';
+export * from './adapters/ssr-adapter';
+export * from './adapters/router-adapter';
