9f7fde9b3a5f43c23e46d4551fd9fe3b0ca1c4fb
TheSkiF4er <cajeerskif4er@gmail.com>
2025-12-11 08:22:24 +0300

    Add files via upload

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  ignorePatterns: ['dist', 'coverage'],
+};
