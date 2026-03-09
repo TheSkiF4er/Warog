d6ca254267aee1131ba40e2323b39cd29c87d9b4
SkiF4er <cajeerskif4er@gmail.com>
2026-03-09 23:19:44 +0300

    Add files via upload

--- examples/counter/index.js
+++ examples/counter/index.js
@@ -1,1 +1,15 @@
+import { batch, derive, signal, watch } from "@warog/core";
 
+const count = signal(0);
+const doubled = derive(() => count.get() * 2);
+
+const stop = watch(() => {
+  console.log(`count=${count.get()} doubled=${doubled.get()}`);
+});
+
+batch(() => {
+  count.set(1);
+  count.update((value) => value + 1);
+});
+
+stop();
