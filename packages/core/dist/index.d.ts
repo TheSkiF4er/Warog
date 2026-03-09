ca55adee35dd5c27d9731c03b4bc3f941d46f6cc
SkiF4er <cajeerskif4er@gmail.com>
2026-03-09 23:32:27 +0300

    Add files via upload

--- packages/core/dist/index.d.ts
+++ packages/core/dist/index.d.ts
@@ -1,1 +1,16 @@
-
+export type Cleanup = () => void;
+export interface ReadonlySignal<T> {
+    get(): T;
+    subscribe(listener: () => void): Cleanup;
+}
+export interface Signal<T> extends ReadonlySignal<T> {
+    set(next: T): void;
+    update(updater: (current: T) => T): void;
+    peek(): T;
+}
+export declare function signal<T>(initial: T): Signal<T>;
+export declare function batch<T>(fn: () => T): T;
+export declare function untrack<T>(fn: () => T): T;
+export declare function watch(effect: () => void | Cleanup): Cleanup;
+export declare function derive<T>(compute: () => T): ReadonlySignal<T>;
+//# sourceMappingURL=index.d.ts.map
